f738bdccb864bcefd4345ab849c54b04813051dc
mrozikinteriors-jpg <mrozikinteriors@gmail.com>
2026-04-07 21:45:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Obsługuje timery w tle gdy aplikacja jest zamknięta
 // ============================================================
 
-const CACHE_NAME = 'prototyp-desk-v3';
+const CACHE_NAME = 'prototyp-desk-v4';
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
