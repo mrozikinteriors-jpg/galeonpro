ced36f849659e33a407ae0526bd7e5bcc49a5484
mrozikinteriors-jpg <mrozikinteriors@gmail.com>
2026-04-15 20:40:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'prototyp-desk-v21';
+const CACHE_NAME = 'prototyp-desk-v22';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
   event.waitUntil(
